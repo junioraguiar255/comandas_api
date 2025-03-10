67141f41d361950de3ae62101c69788edbc8f863
josemar <josemar.aguiar@grupomateus.com>
2025-03-09 23:39:45 -0300

    fase 1 - finalizacao

--- src/app/FuncionarioDAO.py
+++ src/app/FuncionarioDAO.py
@@ -5,19 +5,19 @@ router= APIRouter()
  # Criar as rotas/endpoints: GET, POST, PUT, DELETE
  # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 @router.get("/funcionario/", tags=["Funcionário"])
-def get_funcionario():
+async def get_funcionario():
  return {"msg": "get todos executado"}, 200
 @router.get("/funcionario/{id}", tags=["Funcionário"])
-def get_funcionario(id: int):
+async def get_funcionario(id: int):
  return {"msg": "get um executado"}, 200
 @router.post("/funcionario/", tags=["Funcionário"])
-def post_funcionario(corpo: Funcionario):
+async def post_funcionario(corpo: Funcionario):
  return {"msg": "post executado", "nome": corpo.nome, "cpf": corpo.cpf, "telefone": 
 corpo.telefone}, 200
 @router.put("/funcionario/{id}", tags=["Funcionário"])
-def put_funcionario(id: int, corpo: Funcionario):
+async def put_funcionario(id: int, corpo: Funcionario):
  return {"msg": "put executado", "id":id, "nome": corpo.nome, "cpf": corpo.cpf, "telefone": 
 corpo.telefone}, 200
 @router.delete("/funcionario/{id}", tags=["Funcionário"])
-def delete_funcionario(id: int):
+async def delete_funcionario(id: int):
  return {"msg": "delete executado"}, 200
